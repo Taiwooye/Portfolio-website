9eea8f4c035c9018dca5e0eee55f51b8c7a22c2f
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-07-05 13:32:41 +0100

    text justified

--- src/Certificate.jsx
+++ src/Certificate.jsx
@@ -8,21 +8,18 @@ const Certificate = () => {
         <main >
             <h1>I'm a professional software developer.</h1>
 
-            <p>As a Front-end Developer, i have had the opportunity to work on wide range of projects, helping clients achieve their goals by building web applications that are visually stunning and functionally robust,
-                with my expertise in HTML, CSS, JavaScript and React.
+            <p>As a Front-end Developer, i have had the opportunity to work on wide range of projects, helping clients 
+                achieve their goals by building web applications that are visually stunning and functionally robust, with my expertise in HTML, CSS, JavaScript and React.
                 I have continuously expanded my skill set, keeping up with the latest industry trends and technologies. From HTML, CSS, and JavaScript to frameworks like React.
-                 I am well-versed in a wide range of tools and languages.
-                I am well-equipped to bring to life any design concept while ensuring seamless performance and accessibility.
+                 I am well-versed in a wide range of tools and languages. I am well-equipped to bring to life any design concept while ensuring seamless performance and accessibility.
                  <br />
                  <br />
-                 My passion for clean, efficient and elegant code has helped me develop a keen eye for detail
-                  and a drive for continuous improvement. 
-                  Whether working independently or collaborating with a team, i take pride in delivering high-quality work comfortable working with various content management systems, including WordPress,Shopify, and Squarespace, and i am always looking to expand my skill set and stay up-to date with latest industry trends and best practice.
+                 My passion for clean, efficient and elegant code has helped me develop a keen eye for detail and a drive for continuous improvement.
+                   Whether working independently or collaborating with a team, i take pride in delivering high-quality work comfortable working with various content management systems, including WordPress,Shopify, Squarespace, and i am always looking to expand my skill set and stay up-to date with latest industry trends and best practice.
                  <br />
                  <br />
-                 In addition to my technical skills, i am a strong communicator and collaborator, able to effectively communicate with stakeholders at all lvels of an organization. i am committed to meeting projects deadlines and providing exceptional customer service, and i am always eager to work on new and callenging projects.
-                 In conclusion, my individual skill set in HTML,
-                  Bootstrap, JavaScript, and React equips me with the expertise to build modern, user-friendly, and highly interactive web applications.
+                 In addition to my technical skills, i am a strong communicator and collaborator, able to effectively communicate with stakeholders at all lvels of an organization. i am committed to meeting projects deadlines and providing exceptional customer service, and i am always eager to work on new and callenging projects. 
+                 In conclusion, my individual skill set in HTML, Bootstrap, JavaScript, and React equips me with the expertise to build modern, user-friendly, and highly interactive web applications.
                    I am continuously enhancing my skills and staying up to date with the latest advancements in these technologies to deliver innovative solutions that meet the needs and expectations of clients and users.
         
             </p>
